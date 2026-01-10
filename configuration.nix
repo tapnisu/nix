483a46411acf101bd90bf3570522064e3801d027
Aleksei Rybin <aleksei@tapni.su>
2026-01-10 17:28:07 +0700

    feat: enable steam

--- configuration.nix
+++ configuration.nix
@@ -94,6 +94,10 @@
     openFirewall = true;
   };
 
+  programs.steam.enable = true;
+
+  programs.gamemode.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
