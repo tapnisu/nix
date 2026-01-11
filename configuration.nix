b7842c04c7fba0b3bb7a8abac219e0f7fd0d87c9
Aleksei Rybin <aleksei@tapni.su>
2026-01-11 15:53:55 +0700

    fix: enable xwayland native scaling

--- configuration.nix
+++ configuration.nix
@@ -71,7 +71,7 @@
       extraGSettingsOverridePackages = [ pkgs.mutter ];
       extraGSettingsOverrides = ''
         [org.gnome.mutter]
-        experimental-features=['scale-monitor-framebuffer']
+        experimental-features=['scale-monitor-framebuffer', 'xwayland-native-scaling']
       '';
     };
   };
