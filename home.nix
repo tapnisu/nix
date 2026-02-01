38e59f5c46c7abb5cd7ffb79fab2606c7028933f
Aleksei Rybin <aleksei@tapni.su>
2026-02-01 17:43:11 +0700

    feat: replace vscode with zed

--- home.nix
+++ home.nix
@@ -29,13 +29,6 @@
 
   xdg.configFile."niri/config.kdl".source = ./niri/niri.kdl;
 
-  programs.vscode = {
-    enable = !isWSL;
-    profiles.default = {
-      extensions = with pkgs.vscode-extensions; [];
-    };
-  };
-
   programs.firefox = {
     enable = !isWSL;
     package = pkgs.firefox-esr;
@@ -61,7 +54,7 @@
 
   home.sessionVariables = {
     MOZ_USE_XINPUT2 = "1";
-    EDITOR = "code";
+    EDITOR = "zed";
     BROWSER = "firefox";
     TERMINAL = "alacritty";
   };
@@ -78,9 +71,37 @@
     };
   };
 
+  programs.zed-editor = {
+    enable = true;
+    extensions = [ "nix" "toml" "rust" ];
+    userSettings = {
+      autosave = "on_focus_change";
+      auto_update = false;
+      disable_ai = true;
+      terminal = {
+        font_family = "IosevkaTerm Nerd Font";
+      };
+      ui_font_family = ".ZedSans";
+      telemetry = {
+        diagnostics = false;
+        metrics = false;
+      };
+      ui_font_size = 16;
+      buffer_font_size = 16;
+      theme = {
+        mode = "dark";
+        light = "Gruvbox Light Soft";
+        dark = "Gruvbox Dark Hard";
+      };
+      buffer_font_family = "Iosevka Nerd Font";
+    };
+  };
+
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
     tapciify.packages.${pkgs.stdenv.hostPlatform.system}.default
+
+    zed
 
     keepassxc
     nautilus
@@ -180,7 +201,7 @@
       };
       init.defaultBranch = "main";
       pull.rebase = true;
-      core.editor = "code";
+      core.editor = "zed --wait";
       commit.gpgsign = true;
       tag.gpgsign = true;
     };
--- hosts/common-wsl.nix
+++ hosts/common-wsl.nix
@@ -9,6 +9,4 @@
   wsl.defaultUser = "tapnisu";
 
   wsl.docker-desktop.enable = true;
-
-  programs.nix-ld.enable = true;
 }
