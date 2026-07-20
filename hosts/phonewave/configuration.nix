4b29ec41f0888334a2e604635efacd0e5cfde782
Aleksei Rybin <aleksei@tapni.su>
2026-07-20 15:58:45 +0700

    phonewave: try removing unsupported options

--- hosts/phonewave/configuration.nix
+++ hosts/phonewave/configuration.nix
@@ -1,8 +1,30 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
-  imports = [../common.nix];
-  networking.hostName = "tapnisu-phonewave";
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+    substituters = https://nixos-cache-proxy.cofob.dev
+  '';
+
+  environment.packages = with pkgs; [
+    neovim
+    wget
+    git
+    fastfetch
+    zsh
+  ];
+
+  environment.sessionVariables = {
+    EDITOR = "nvim";
+    LANG = "ru_RU.UTF-8";
+  };
+
+  user.shell = pkgs.zsh;
+
+  environment.etcBackupExtension = ".bak";
+
+  system.stateVersion = "25.11";
 }
